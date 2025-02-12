5ea438b5379c3ccbc8529eb1520c7e58b03ae901
Tismaa78 <mathisroussel78@gmail.com>
2025-02-12 23:57:37 +0100

    Mise à jour du fichier index.js avec redirenone

--- Portfolio/src/Data.js
+++ Portfolio/src/Data.js
@@ -2,15 +2,14 @@ const Data = {
   WebsiteTitle: "Samy | Flyers",
   HeaderTitle: "Samy Flyers",
   FooterText: "© 2025 Samy Flyers.",
-  AboutPhone: "Samy",
-  AboutPhoneNumber: "tel:+33683738897", // Ajout du numéro de téléphone
-  AboutEmailSubject: "Subject of the Email",
   AboutHeading: "Informations",
+  AboutPhone: "+33 6 83 73 88 97", // Ajout du numéro de téléphone en clair
+  AboutPhoneNumber: "tel:+33683738897", // Format cliquable pour les liens
   AboutTextParagraph1:
     "30€ le flyer, livraison en 72/96 heures + 10€ livraison urgence en 24 heures.",
   AboutTextParagraph2:
-    " PACK VISION : 1 logo + 1 filtre + 1 flyers pour 60€, livraison en 72/96 heures.  ",
-  AboutButtonText: "Contactez moi !",
+    "PACK VISION : 1 logo + 1 filtre + 1 flyer pour 60€, livraison en 72/96 heures.",
+  AboutButtonText: "Appelez-moi !", // Texte du bouton mis à jour pour l'appel
 };
 
 export default Data;
