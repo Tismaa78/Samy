b8f27895bc21bba03e46735370d45513b825b0ec
Tismaa78 <mathisroussel78@gmail.com>
2025-02-12 19:12:55 +0100

    Initial commit

---
+++ server/ArtistPortfolio/src/Data.js
@@ -0,0 +1,15 @@
+const Data = {
+  WebsiteTitle: "Portfolio | Artists",
+  HeaderTitle: "Artist's Portfolio",
+  FooterText: "© 2025 Samy Flyers.",
+  AboutEmail: "Your Email Address",
+  AboutEmailSubject: "Subject of the Email",
+  AboutHeading: "Informations",
+  AboutTextParagraph1:
+    "30€ le flyer, livraison en 72/96 heures + 10€ livraison urgence en 24 heures.",
+  AboutTextParagraph2:
+    " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  AboutButtonText: "Say Hey!",
+};
+
+export default Data;
